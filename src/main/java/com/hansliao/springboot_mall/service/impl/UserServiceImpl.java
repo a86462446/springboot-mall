069605cee090beb369ef82c47463b40d3d42659f
Hans <a0903150698@gmail.com>
2024-08-06 11:18:31 +0800

    feat: check email and hide return password

--- src/main/java/com/hansliao/springboot_mall/service/impl/UserServiceImpl.java
+++ src/main/java/com/hansliao/springboot_mall/service/impl/UserServiceImpl.java
@@ -1,7 +1,11 @@
 package com.hansliao.springboot_mall.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.hansliao.springboot_mall.dao.UserDao;
 import com.hansliao.springboot_mall.dto.UserRegisterRequest;
@@ -10,6 +14,8 @@ import com.hansliao.springboot_mall.service.UserService;
 
 @Component
 public class UserServiceImpl implements UserService{
+
+    private final static Logger log= LoggerFactory.getLogger(UserServiceImpl.class);
     
     @Autowired
     private UserDao userDao;
@@ -21,6 +27,17 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest){
+
+        // 檢查註冊的email
+        User user= userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+        if(user!= null){
+            log.warn("該email {} 已經被註冊", userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        
+        // 創建帳號
         return userDao.createUser(userRegisterRequest);
+        
     }
 }
